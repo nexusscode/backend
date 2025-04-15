98389e25672906ce18b921e5fca2eac3c340bd13
hongsy521 <seoyeong0521@naver.com>
2025-04-15 17:17:21 +0900

    feat: apllication crud api

--- src/main/java/org/nexusscode/backend/application/domain/Career.java
+++ src/main/java/org/nexusscode/backend/application/domain/Career.java
@@ -1,5 +1,16 @@
 package org.nexusscode.backend.application.domain;
 
 public enum Career {
-    NEW, EXPERIENCED
+    NEW("신입"),
+    EXPERIENCED("경력");
+
+    private final String career;
+
+    Career(String career) {
+        this.career = career;
+    }
+
+    public String getCareer() {
+        return career;
+    }
 }
