0e5503da761fb526ad6ace356cdf8316a7466d7f
hongsy521 <seoyeong0521@naver.com>
2025-04-30 22:31:22 +0900

    feat: 공고엔티티 생성자 빌더패턴으로 변경

--- src/main/java/org/nexusscode/backend/application/domain/JobApplication.java
+++ src/main/java/org/nexusscode/backend/application/domain/JobApplication.java
@@ -1,24 +1,25 @@
 package org.nexusscode.backend.application.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.nexusscode.backend.application.dto.ApplicationRequestDto;
-import org.nexusscode.backend.application.dto.ApplicationUpdateRequestDto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.nexusscode.backend.global.Timestamped;
 import org.nexusscode.backend.user.domain.User;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@EntityListeners(AuditingEntityListener.class)
 public class JobApplication extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -54,4 +55,19 @@ public class JobApplication extends Timestamped {
 
     @Column(name = "required_education_level")
     private String requiredEducationLevel;
+
+    @Builder
+    public JobApplication(String saraminJobId, String companyName, String jobTitle, Status status,
+        LocalDateTime expirationDate, String experienceLevel, String jobCode, String jobType,
+        String requiredEducationLevel) {
+        this.saraminJobId = saraminJobId;
+        this.companyName = companyName;
+        this.jobTitle = jobTitle;
+        this.status = status;
+        this.expirationDate = expirationDate;
+        this.experienceLevel = experienceLevel;
+        this.jobCode = jobCode;
+        this.jobType = jobType;
+        this.requiredEducationLevel = requiredEducationLevel;
+    }
 }
