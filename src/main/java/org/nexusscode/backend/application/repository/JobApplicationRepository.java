fbe2a2713df249bc44f55bbe30fb0af9fda21459
hongsy521 <seoyeong0521@naver.com>
2025-05-11 21:25:12 +0900

    feat: 공고 검색 api 생성

--- src/main/java/org/nexusscode/backend/application/repository/JobApplicationRepository.java
+++ src/main/java/org/nexusscode/backend/application/repository/JobApplicationRepository.java
@@ -1,8 +1,10 @@
 package org.nexusscode.backend.application.repository;
 
+import java.util.List;
 import org.nexusscode.backend.application.domain.JobApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
 
+    List<JobApplication> findByCompanyNameContainingIgnoreCaseOrJobTitleContainingIgnoreCase(String searchWord, String searchWord1);
 }
