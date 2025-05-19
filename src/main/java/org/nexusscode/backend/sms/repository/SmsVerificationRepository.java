a282865323ad00a26778c288ad4640e10a888211
hongsy521 <seoyeong0521@naver.com>
2025-05-19 21:54:54 +0900

    feat: sms 인증 api 생성

--- src/main/java/org/nexusscode/backend/sms/repository/SmsVerificationRepository.java
+++ src/main/java/org/nexusscode/backend/sms/repository/SmsVerificationRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SmsVerificationRepository extends JpaRepository<SmsVerification,Long> {
 
+    SmsVerification findByPhoneNumberAndVerificationCode(String phoneNumber, String code);
 }
