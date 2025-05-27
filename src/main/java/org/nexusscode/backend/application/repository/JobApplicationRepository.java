2ed1dc09253363e143a53c8fb484bfc2dbedd5bb
hongsy521 <seoyeong0521@naver.com>
2025-05-27 21:55:08 +0900

    feat: 공고 검색 JPA 쿼리메소드 @Query 명시

--- src/main/java/org/nexusscode/backend/application/repository/JobApplicationRepository.java
+++ src/main/java/org/nexusscode/backend/application/repository/JobApplicationRepository.java
@@ -1,13 +1,25 @@
 package org.nexusscode.backend.application.repository;
 
-import java.util.List;
 import org.nexusscode.backend.application.domain.JobApplication;
+import org.nexusscode.backend.user.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
 
-    Page<JobApplication> findByCompanyNameContainingIgnoreCaseOrJobTitleContainingIgnoreCase(String searchWord, String searchWord1,
+    @Query("""
+    SELECT a FROM JobApplication a
+    WHERE a.user = :user AND (
+          LOWER(a.companyName) LIKE LOWER(CONCAT('%', :searchWord, '%')) OR
+          LOWER(a.jobTitle) LIKE LOWER(CONCAT('%', :searchWord, '%'))
+    )
+    """)
+    Page<JobApplication> searchByUserAndCompanyOrTitle(@Param("user") User user,
+        @Param("searchWord") String searchWord,
         Pageable pageable);
+
+    Page<JobApplication> findAllByUser(User user, Pageable pageable);
 }
