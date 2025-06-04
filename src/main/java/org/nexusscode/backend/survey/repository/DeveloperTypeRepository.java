ea57cdc237c4a80da48509432a0164b335c6221f
hongsy521 <seoyeong0521@naver.com>
2025-06-04 22:38:16 +0900

    feat: disc 유형 4가지, 개발 유형 4가지 각각의 유형 객체 미리 저장

---
+++ src/main/java/org/nexusscode/backend/survey/repository/DeveloperTypeRepository.java
@@ -0,0 +1,8 @@
+package org.nexusscode.backend.survey.repository;
+
+import org.nexusscode.backend.survey.domain.DeveloperType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeveloperTypeRepository extends JpaRepository<DeveloperType,Long> {
+
+}
---
+++ src/main/java/org/nexusscode/backend/survey/repository/DiscTypeRepository.java
@@ -0,0 +1,8 @@
+package org.nexusscode.backend.survey.repository;
+
+import org.nexusscode.backend.survey.domain.DiscType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiscTypeRepository extends JpaRepository<DiscType,Long> {
+
+}
