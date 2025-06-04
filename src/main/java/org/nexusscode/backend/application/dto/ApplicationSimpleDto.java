4cdb88f04ad3ff722f03b8e60cd4aef1febdcbf8
hongsy521 <seoyeong0521@naver.com>
2025-06-05 01:18:07 +0900

    feat: 공고 생성시 자소서 바로 생성 -> 공고 조회시 자소서 피드백 유무 확인 가능

--- src/main/java/org/nexusscode/backend/application/dto/ApplicationSimpleDto.java
+++ src/main/java/org/nexusscode/backend/application/dto/ApplicationSimpleDto.java
@@ -3,6 +3,7 @@ package org.nexusscode.backend.application.dto;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import org.nexusscode.backend.application.domain.JobApplication;
+import org.nexusscode.backend.resume.domain.ResumeFeedbackStatus;
 
 @Getter
 public class ApplicationSimpleDto {
@@ -11,12 +12,14 @@ public class ApplicationSimpleDto {
     private String jobTitle;
     private String experienceLevel;
     private LocalDateTime createdAt;
+    private String ResumeFeedbackStatus;
 
-    public ApplicationSimpleDto(JobApplication jobApplication) {
+    public ApplicationSimpleDto(JobApplication jobApplication, ResumeFeedbackStatus resumeFeedbackStatus) {
         this.applicationId= jobApplication.getId();
         this.companyName = jobApplication.getCompanyName();
         this.jobTitle = jobApplication.getJobTitle();
         this.experienceLevel = jobApplication.getExperienceLevel();
         this.createdAt = jobApplication.getCreatedAt();
+        this.ResumeFeedbackStatus=resumeFeedbackStatus.getStatus();
     }
 }
